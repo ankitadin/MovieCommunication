7be6d865c171d6a30d9e0394930b1af69c61bb41
anujdin <44108451+anujdin@users.noreply.github.com>
2020-10-30 11:01:50 +0530

    movie Eureka

---
+++ MovieCommunication/src/main/java/com/my/ser/EurekaMyServer.java
@@ -0,0 +1,16 @@
+package com.my.ser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMyServer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMyServer.class, args);
+	}
+
+}
